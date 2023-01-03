2446d2b6e097ce2e93ed3888911e9e50652ded0c
timoyo93 <timo.deutzmann@gmx.de>
2023-01-03 15:18:00 +0100

    setup ktor api

--- kotlin-ktor/src/main/kotlin/com/example/plugins/Routing.kt
+++ kotlin-ktor/src/main/kotlin/com/example/plugins/Routing.kt
@@ -4,7 +4,7 @@ import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
-import io.ktor.server.request.*
+import kotlinx.serialization.Serializable
 
 fun Application.configureRouting() {
 
@@ -12,5 +12,36 @@ fun Application.configureRouting() {
         get("/") {
             call.respondText("Hello World!")
         }
+        get("/healthcheck") {
+            call.respondText("\"Ok\"", ContentType.Application.Json, HttpStatusCode.OK)
+        }
+        route("/employees") {
+            get {
+                val employees = listOf(
+                    Employee(1, "John", "Doe"),
+                    Employee(2, "Peter", "Parker"),
+                    Employee(3, "Tony", "Stark"))
+                call.respond(HttpStatusCode.OK, employees)
+            }
+            post {
+                val n = 40
+                val fib = fibonacci(n)
+                call.respond(HttpStatusCode.OK, fib)
+            }
+        }
     }
 }
+
+fun fibonacci(n: Int): Int {
+    if (n <= 1) {
+        return n
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+@Serializable
+data class Employee(
+    val id: Int,
+    val firstname: String,
+    val lastname: String,
+)
