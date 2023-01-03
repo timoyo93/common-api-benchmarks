ce52f9bbc92a7ea451f2c49601fe1104ff074ed8
timoyo93 <timo.deutzmann@gmx.de>
2023-01-03 15:18:15 +0100

    setup spring boot api

---
+++ kotlin-spring/src/main/java/com/example/kotlinspring/Employee.java
@@ -0,0 +1,12 @@
+package com.example.kotlinspring;
+
+public class Employee {
+    public Employee(int id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+    public int id;
+    public String firstname;
+    public String lastname;
+}
--- kotlin-spring/src/main/java/com/example/kotlinspring/KotlinSpringApplication.java
+++ kotlin-spring/src/main/java/com/example/kotlinspring/KotlinSpringApplication.java
@@ -2,8 +2,14 @@ package com.example.kotlinspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
 
 @SpringBootApplication
 @RestController
@@ -13,8 +19,38 @@ public class KotlinSpringApplication {
         SpringApplication.run(KotlinSpringApplication.class, args);
     }
 
+     int Fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+
     @GetMapping("/")
     public String sayHello() {
         return "Hello world!";
     }
+
+    @GetMapping(value = "/healthcheck", produces = "application/json")
+    public ResponseEntity<String> healthcheck() {
+        return new ResponseEntity<>("\"Ok\"", HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/employees", produces = "application/json")
+    public ResponseEntity<List<Employee>> getEmployees() {
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(1, "John", "Doe"));
+        employees.add(new Employee(2, "Peter", "Parker"));
+        employees.add(new Employee(3, "Tony", "Stark"));
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/employees", produces = "application/json")
+    public ResponseEntity<Integer> createEmployee() {
+        int n = 40;
+        int fib = Fibonacci(n);
+        return new ResponseEntity<>(fib, HttpStatus.OK);
+    }
+
 }
+
