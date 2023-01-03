2c3b45f1edf9c65059e3172296d44dd4983d20ed
timoyo93 <timo.deutzmann@gmx.de>
2023-01-03 15:17:12 +0100

    setup .net minimal api

---
+++ dotnet-minimal-api/Employee.cs
@@ -0,0 +1,8 @@
+namespace dotnet_minimal_api;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string? Firstname { get; set; }
+    public string? Lastname { get; set; }
+};
--- dotnet-minimal-api/Program.cs
+++ dotnet-minimal-api/Program.cs
@@ -1,6 +1,38 @@
+using dotnet_minimal_api;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+static int Fibonacci(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+app.MapPost("/employees", () =>
+{
+    var n = 40;
+    var fib = Fibonacci(n);
+
+    return Results.Ok(fib);
+});
+
+app.MapGet("/employees", () =>
+{
+    var employees = new List<Employee>()
+    {
+        new() { Id = 1, Firstname = "John", Lastname = "Doe" },
+        new() { Id = 2, Firstname = "Peter", Lastname = "Parker" },
+        new() { Id = 3, Firstname = "Tony", Lastname = "Stark" }
+    };
+    return Results.Ok(employees);
+});
+
+app.MapGet("/healthcheck", () => Results.Ok("Ok"));
+
 
 app.Run("http://localhost:8040");
