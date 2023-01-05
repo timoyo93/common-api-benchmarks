b4aca9286af4070792a0a67d74a2282cf5c0a4e9
timoyo93 <timo.deutzmann@gmx.de>
2023-01-05 14:23:10 +0100

    finished healthcheck ui

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,45 +1,81 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import Card from './lib/Card.svelte';
+  import './app.css';
+
+  const services: { name: string; baseUrl: string }[] = [
+    {
+      name: 'dotnet-api',
+      baseUrl: import.meta.env.VITE_DOTNET_URL,
+    },
+    {
+      name: 'dotnet-minimal-api',
+      baseUrl: import.meta.env.VITE_DOTNET_MINIMAL_URL,
+    },
+    {
+      name: 'go-echo',
+      baseUrl: import.meta.env.VITE_GO_URL,
+    },
+    {
+      name: 'kotlin-ktor',
+      baseUrl: import.meta.env.VITE_KTOR_URL,
+    },
+    {
+      name: 'kotlin-spring',
+      baseUrl: import.meta.env.VITE_SPRING_URL,
+    },
+    {
+      name: 'rust-rocket',
+      baseUrl: import.meta.env.VITE_RUST_URL,
+    },
+  ];
+
+  let healthCheckInterval = '10';
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+<main class="main-body">
+  <div
+    class="py-8 px-16 w-full flex flex-col items-center justify-center h-full"
+  >
+    <h1 class="text-center text-6xl mb-10 uppercase font-black heading">
+      Backend Status
+    </h1>
+    <div class="mb-10 text-lg">
+      <label for="seconds">Do healthcheck every</label>
+      <select
+        bind:value={healthCheckInterval}
+        name="seconds"
+        id="seconds"
+        autocomplete="off"
+      >
+        <option value="5" selected>5</option>
+        <option value="10">10</option>
+        <option value="15">15</option>
+        <option value="20">20</option>
+        <option value="25">25</option>
+        <option value="30">30</option>
+      </select>
+      <label for="seconds">seconds</label>
+    </div>
+    <div class="grid grid-cols-3 gap-6">
+      {#each services as service}
+        <Card
+          serviceName={service.name}
+          baseUrl={service.baseUrl}
+          checkIntervallInSeconds={parseInt(healthCheckInterval)}
+        />
+      {/each}
+    </div>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
+  .main-body {
+    width: 100vw;
+    height: 100vh;
+    background: rgba(237, 242, 247);
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+
+  .heading {
+    text-shadow: 0 0 40px gray;
   }
 </style>
---
+++ ui/src/lib/Card.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+  import { onDestroy } from 'svelte';
+  import { Circle } from 'svelte-loading-spinners';
+
+  export let serviceName: string = '';
+  export let baseUrl: string = '';
+  export let checkIntervallInSeconds: number = 15;
+
+  let isHealthy = false;
+  let time;
+  let healthcheck;
+  let interval;
+
+  async function doHealthcheck() {
+    // Add timeout to reduce flickerung in Browser since healthcheck is fast
+    const res: Response = await fetch(`${baseUrl}/healthcheck`).then(
+      (v) =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(v);
+          }, 500);
+        })
+    );
+
+    if (res.ok) {
+      isHealthy = true;
+    } else {
+      isHealthy = false;
+    }
+  }
+
+  onDestroy(() => {
+    clearInterval(interval);
+  });
+
+  $: {
+    healthcheck = doHealthcheck();
+    time = new Date().toLocaleTimeString();
+    clearInterval(interval);
+    interval = setInterval(() => {
+      healthcheck = doHealthcheck();
+      time = new Date().toLocaleTimeString();
+    }, checkIntervallInSeconds * 1000);
+  }
+</script>
+
+<section
+  class="bg-white w-[250px] h-[250px] p-4 neumo-card flex flex-col items-center justify-between"
+>
+  <h1 class="uppercase text-center font-semibold">{serviceName}</h1>
+  {#await healthcheck}
+    <div>
+      <Circle size="30" unit="px" />
+    </div>
+  {:then}
+    {#if isHealthy}
+      <div class="bg-green-600 p-8 text-lg text-gray-200 health-box healthy">
+        <p class="text-center">OK</p>
+      </div>
+    {:else}
+      <div class="bg-red-600 health-box unhealthy">
+        <p class="text-center text-4xl">😢</p>
+      </div>
+    {/if}
+  {:catch error}
+    <p>{error.message}</p>
+  {/await}
+  <p class="text-sm text-gray-600">Last checked: {time}</p>
+</section>
+
+<style>
+  .neumo-card {
+    border-radius: 30px;
+    box-shadow: 5px 5px 10px #b3b3b3, -5px -5px 10px #fff;
+  }
+  .health-box {
+    width: 100px;
+    @apply p-8 text-lg text-gray-200;
+    border-radius: 10px;
+  }
+  .healthy {
+    background: linear-gradient(145deg, #18ae4f, #149343);
+    box-shadow: 3px 3px 10px #0f7033, -3px -3px 10px #1dd661;
+  }
+  .unhealthy {
+    background: linear-gradient(145deg, #eb2929, #c62222);
+    box-shadow: 3px 3px 10px #981a1a, -3px -3px 10px #ff3232;
+  }
+</style>
